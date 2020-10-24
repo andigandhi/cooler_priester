28ef6989c1492b9e514b2b5bb452d237b27213d5
Andreas G <andigrasser13@gmail.com>
2020-10-24 17:32:59 +0200

    Improvements of the Frontend

--- Client/spiel.js
+++ Client/spiel.js
@@ -1,4 +1,5 @@
-// JavaScript Document
+var serverIP = "localhost";
+
 
 var farben = [
 	"C",
@@ -28,14 +29,15 @@ var nextCard = -1;
 var hand = [];
 var stapel = [];
 var offen = [];
-var name = "";
+var username = "";
 var andereSpieler = [];
 
-var exampleSocket = null;
+var webSocket = null;
 
 
 
 function numberToFile(nr) {
+	"use strict";
 	if (!nr && nr!==0) {
 		return "x.png";
 	}
@@ -47,11 +49,13 @@ function numberToFile(nr) {
 
 
 function renderCard(cardId, handNr) {
+	"use strict";
 	return "<img class=\"handKarte\" src=\"karten/"+numberToFile(cardId)+"\" alt=\"\" onClick=\"ausspielen("+cardId+", "+handNr+")\">";
 }
 
 
 function renderHand() {
+	"use strict";
 	var str = "";
 	for(var i=0; i<hand.length; i++) {
 		str += renderCard(hand[i], i);
@@ -60,6 +64,7 @@ function renderHand() {
 }
 
 function renderAblage() {
+	"use strict";
 	document.getElementById("ablage1").src = "karten/"+numberToFile(stapel[stapel.length-4]);
 	document.getElementById("ablage2").src = "karten/"+numberToFile(stapel[stapel.length-3]);
 	document.getElementById("ablage3").src = "karten/"+numberToFile(stapel[stapel.length-2]);
@@ -67,59 +72,73 @@ function renderAblage() {
 }
 
 function renderOffen() {
+	"use strict";
 	document.getElementById("letzteKarten1").src = "karten/"+numberToFile(offen[0]);
 	document.getElementById("letzteKarten2").src = "karten/"+numberToFile(offen[1]);
 	document.getElementById("letzteKarten3").src = "karten/"+numberToFile(offen[2]);
 }
 
 function renderAndereSpieler() {
+	"use strict";
 	for(var i = 0; i<andereSpieler.length/3; i++) {
-		str = "";
+		var str = "";
 		str += andereSpieler[i*3] + " (" + andereSpieler[i*3+1] + " Karten in der Hand)<br>";
 		for (var j = 0; j<andereSpieler[i*3+2].length; j++) {
-			str += "<img src=\"karten/"+numberToFile(andereSpieler[i*3+2][j])+"\" alt=\"\" width=\"2%\">";
+			str += "<img src=\"karten/"+numberToFile(andereSpieler[i*3+2][j])+"\" alt=\"\" width=\"15%\">";
 		}
 		document.getElementById("spieler"+i).innerHTML = str;
 	}
 }
 
 function render() {
+	"use strict";
 	renderHand();
 	renderAblage();
 	renderOffen();
-	renderAndereSpieler()
+	renderAndereSpieler();
+	
+	if (dran) {
+		document.getElementById("hand").style.filter = "grayscale(0%)";
+		document.getElementById("letzteKarten").style.filter = "grayscale(0%)";
+	} else {
+		document.getElementById("hand").style.filter = "grayscale(70%)";
+		document.getElementById("letzteKarten").style.filter = "grayscale(70%)";
+	}
 }
 
 
 function ausspielen(nr, handNr) {
+	"use strict";
 	if (dran) {
-		exampleSocket.send(name+";"+handNr); 
+		webSocket.send(username+";"+handNr); 
 	}
 	console.log(nr);
 	dran = !dran;
 }
 
 function aufnehmen() {
-	exampleSocket.send(name+";nehme"); 
+	"use strict";
+	webSocket.send(username+";nehme"); 
 }
 
 
 
 function login() {
-	name = document.getElementById("nameText").text;
+	"use strict";
+	username = document.getElementById("nameText").value;
 	document.getElementById("startSeite").style.display = "none";
 	document.getElementById("spielSeite").style.display = "inline";
-	//name = prompt("Wie ist denn bitte dein Name?", "");
+	//username = prompt("Wie ist denn bitte dein Name?", "");
 	
-	exampleSocket = new WebSocket("ws://localhost:8442");
+	webSocket = new WebSocket('ws://'+serverIP+':8442');
 	
 	renderHand();
 	
-	exampleSocket.onopen = function (event) {
-	  exampleSocket.send(name); 
+	webSocket.onopen = function (event) {
+	  webSocket.send(username); 
 	};
 	
-	exampleSocket.onmessage = function (event) {
+	webSocket.onmessage = function (event) {
 		var msg = JSON.parse(event.data);
 		
 		dran = msg.Dran;
@@ -127,9 +146,28 @@ function login() {
 		nextCard = msg.Ziehen;
 		stapel = msg.Ablage;
 		offen = msg.Offen;
-		andereSpieler = msg.Andere
+		andereSpieler = msg.Andere;
 		
 		render();
 	};
+	
+}
+
+function loginTest() {
+	"use strict";
+	username = document.getElementById("nameText").value;
+	document.getElementById("startSeite").style.display = "none";
+	document.getElementById("spielSeite").style.display = "inline";
+	
+	renderHand();
+		
+	dran = true;
+	hand = [46, 11, 39];
+	nextCard = 6;
+	stapel = [25,31,19,2];
+	offen = [41, 18, 21];
+	andereSpieler = ["bot0", 3, [47, 12, 28], "bot1", 3, [22, 17, 34], "bot2", 3, [1, 14, 13]];
+
+	render();
 	
 }
